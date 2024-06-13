299ee094ebd289563dc4d5f7463b6b6772980402
Hminchae <hminchae@gmail.com>
2024-06-13 18:29:17 +0900

    Add : 시드파일 생성 및 초기 셋팅
    - 최소타겟 15.0
    - Portrait 단일 지원
    - 라이트모드 단일 지원

---
+++ SeeTheGood/SeeTheGood/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SeeTheGood
+//
+//  Created by 황민채 on 6/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
+
