a8b12bf3784107f3c2dd4c76b595c96499b4b621
Hminchae <hminchae@gmail.com>
2024-06-13 19:08:19 +0900

    Add : 에셋 추가, 컬러에셋 추가, 열거형 정리

---
+++ SeeTheGood/SeeTheGood/Enum/ViewConstant.swift
@@ -0,0 +1,27 @@
+//
+//  ViewStyle.swift
+//  SeeTheGood
+//
+//  Created by 황민채 on 6/13/24.
+//
+
+import UIKit
+
+enum ViewConstant {
+    enum Font {
+        static let normal13 = UIFont.systemFont(ofSize: 13)
+        static let normal14 = UIFont.systemFont(ofSize: 14)
+        static let normal15 = UIFont.systemFont(ofSize: 15)
+        static let normal16  = UIFont.systemFont(ofSize: 16)
+        
+        static let bold13 = UIFont.boldSystemFont(ofSize: 13)
+        static let bold14 = UIFont.boldSystemFont(ofSize: 14)
+        static let bold15 = UIFont.boldSystemFont(ofSize: 15)
+        static let bold16  = UIFont.boldSystemFont(ofSize: 16)
+    }
+    
+    enum BorderWidth {
+        static let selectedProfile = 3
+        static let unSelectedProfile = 1
+    }
+}
