900f877ace76e5d38af065c9d79b23416ebe9905
Hminchae <hminchae@gmail.com>
2024-06-15 05:42:50 +0900

    Feat : 검색 결과 화면에 뿌려주는 기능 구현

---
+++ SeeTheGood/SeeTheGood/Search/Cell/SearchResultCollectionViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  SearchResultCollectionViewCell.swift
+//  SeeTheGood
+//
+//  Created by 황민채 on 6/15/24.
+//
+
+import UIKit
+
+class SearchResultCollectionViewCell: UICollectionViewCell {
+    
+    let mainImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 5
+        
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    
+        contentView.addSubview(mainImageView)
+        
+        mainImageView.snp.makeConstraints { make in
+            make.edges.equalTo(contentView.snp.edges)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- SeeTheGood/SeeTheGood/Search/SearchResultViewController.swift
+++ SeeTheGood/SeeTheGood/Search/SearchResultViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 import Alamofire
 import SnapKit
+import Kingfisher
 
 class SearchResultViewController: UIViewController {
     
     var searchWord: String?
     var sortWay: String = "sim"
+    var page = 1
+    
+    lazy var currentSearchQueryTotalPage: Int = {
+        if responseList.display != 0 {
+            return responseList.total / responseList.display
+        }
+        return 0
+    }()
+    
+    var isEnd: Bool { // page 가 토탈 페이지에 도달했을 때
+        get {
+            return currentSearchQueryTotalPage == page ? true : false
+        }
+    }
     
     private var responseList = Search(lastBuildDate: "",
                                       total: 0,
@@ -42,33 +57,11 @@ class SearchResultViewController: UIViewController {
     }()
     
     lazy private var sortSimButton = selectedButtonSet("정확도")
-    
-    func selectedButtonSet(_ title: String) -> UIButton {
-        let button = UIButton()
-        button.layer.cornerRadius = 15
-        button.backgroundColor = .firstGray
-        button.setTitle(title, for: .normal)
-        button.titleLabel?.font = ViewConstant.Font.normal14
-        
-        return button
-    }
-    
-    func unSelectedButtonSet(_ title: String) -> UIButton {
-        let button = UIButton()
-        button.backgroundColor = .white
-        button.setTitle(title, for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.titleLabel?.font = ViewConstant.Font.normal14
-        button.layer.borderColor = UIColor.thirdGray.cgColor
-        button.layer.borderWidth = 1
-        button.layer.cornerRadius = 15
-        
-        return button
-    }
-    
     lazy private var sortDateButton = unSelectedButtonSet("날짜순")
     lazy private var sortDscButton = unSelectedButtonSet("가격높은순")
     lazy private var sortAscButton = unSelectedButtonSet("가격낮은순")
+    
+    lazy private var collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout())
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -77,18 +70,28 @@ class SearchResultViewController: UIViewController {
         
         configureAsyncTask()
         configureStackView()
+        configureCollectionView()
         configureView()
     }
     
-    func configureView() {
+    private func configureCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.prefetchDataSource = self
+        collectionView.backgroundColor = .clear
+        
+        collectionView.register(SearchResultCollectionViewCell.self, forCellWithReuseIdentifier: SearchResultCollectionViewCell.identifier)
+    }
+    private func configureView() {
         view.addSubview(topLineView)
         view.addSubview(totalSearchResultLabel)
         view.addSubview(sortStackView)
+        view.addSubview(collectionView)
         
         configureLayout()
     }
     
-    func configureLayout() {
+    private func configureLayout() {
         topLineView.snp.makeConstraints { make in
             make.horizontalEdges.top.equalTo(view.safeAreaLayoutGuide)
             make.height.equalTo(1)
@@ -106,21 +109,24 @@ class SearchResultViewController: UIViewController {
             make.trailing.lessThanOrEqualTo(view.snp.trailing).inset(UIScreen.main.bounds.width - 310)
             make.height.equalTo(30)
         }
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(sortStackView.snp.bottom).offset(10)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(10)
+            make.bottom.equalTo(view.snp.bottom)
+        }
     }
     
-    func configureAsyncTask() {
+    private func configureAsyncTask() {
         if let target = searchWord {
             navigationItem.title = target
             Task {
-                if let value = await callRequest(query: target, sort: "sim") {
-                    responseList = value
-                    totalSearchResultLabel.text = "\(responseList.total)개의 검색 결과"
-                }
+                await callRequest(query: target, sort: "sim", page: page)
             }
         }
     }
     
-    func configureStackView() {
+    private func configureStackView() {
         sortStackView.addArrangedSubview(sortSimButton)
         sortStackView.addArrangedSubview(sortDateButton)
         sortStackView.addArrangedSubview(sortDscButton)
@@ -142,12 +148,53 @@ class SearchResultViewController: UIViewController {
             make.width.equalTo(80)
         }
     }
+    
+    private func selectedButtonSet(_ title: String) -> UIButton {
+        let button = UIButton()
+        button.layer.cornerRadius = 15
+        button.backgroundColor = .firstGray
+        button.setTitle(title, for: .normal)
+        button.titleLabel?.font = ViewConstant.Font.normal14
+        
+        return button
+    }
+    
+    private func unSelectedButtonSet(_ title: String) -> UIButton {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = ViewConstant.Font.normal14
+        button.layer.borderColor = UIColor.thirdGray.cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 15
+        
+        return button
+    }
+    
+    private func collectionViewLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        let sectionSpacing: CGFloat = 15
+        let cellSpacing: CGFloat = 15
+        let width = UIScreen.main.bounds.width - (sectionSpacing * 2) - (cellSpacing * 3)
+        
+        layout.itemSize = CGSize(width: width/2, height: width * 1.5)
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = cellSpacing
+        layout.minimumInteritemSpacing = cellSpacing
+        layout.sectionInset = UIEdgeInsets(top: sectionSpacing,
+                                           left: sectionSpacing,
+                                           bottom: sectionSpacing,
+                                           right: sectionSpacing)
+        
+        return layout
+    }
 }
 
 // 네트워크
 extension SearchResultViewController {
     
-    func callRequest(query: String, sort: String) async -> Search? {
+    func callRequest(query: String, sort: String, page: Int) async {
         let url = "\(APIKey.url.rawValue)?query=\(query)"
         let header: HTTPHeaders = [
             "X-Naver-Client-Id": APIKey.clientID.rawValue,
@@ -155,18 +202,73 @@ extension SearchResultViewController {
         ]
         let para: Parameters = [
             "query": query,
-            "sort": sort
+            "sort": sort,
+            "page": page,
         ]
-        do {
-            let response = try await AF.request(url,
-                                                method: .get,
-                                                parameters: para,
-                                                headers: header)
-                .serializingDecodable(Search.self).value
-            return response
-        } catch {
-            print("네트워크 오류")
-            return nil
+        
+        AF.request(url,
+                   method: .get,
+                   parameters: para,
+                   headers: header).responseDecodable(of: Search.self) { response in
+            switch response.result {
+            case .success(let value):
+                print("SUCCESS")
+                self.responseList.items.append(contentsOf: value.items)
+                self.totalSearchResultLabel.text = "\(value.total)개의 검색 결과"
+                self.collectionView.reloadData()
+            case .failure(let error):
+                print("Failed")
+                print(error)
+            }
+        }
+    }
+}
+
+extension SearchResultViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print(#function)
+        print(responseList)
+        print(responseList.items.count)
+        return responseList.items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        print(#function)
+        let data = responseList.items[indexPath.row]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchResultCollectionViewCell.identifier, for: indexPath) as! SearchResultCollectionViewCell
+        let imageUrl = URL(string: data.image)
+        cell.mainImageView.kf.setImage(with: imageUrl)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension SearchResultViewController: UICollectionViewDataSourcePrefetching {
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        indexPaths.forEach { indexPath in
+            if responseList.items.count - 2 == indexPath.row && !isEnd {
+                page += 1
+                Task {
+                    if let searchWord = searchWord {
+                        await callRequest(query: searchWord, sort: sortWay, page: page)
+                    }
+                    print(page)
+                }
+            }
+        }
+    }
+    
+    // 취소기능
+    func collectionView(_ collectionView: UICollectionView, cancelPrefetchingForItemsAt indexPaths: [IndexPath]) {
+        print("취소", indexPaths)
+        indexPaths.forEach { indexPath in
+            if let cell = collectionView.cellForItem(at: indexPath) as? SearchResultCollectionViewCell {
+                cell.mainImageView.kf.cancelDownloadTask()
+            }
         }
     }
 }
