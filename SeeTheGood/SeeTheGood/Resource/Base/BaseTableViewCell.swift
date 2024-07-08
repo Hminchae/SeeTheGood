b8ae4a40c9ed7caefec6fe4ad06e1aa0cbeb67aa
Hminchae <hminchae@gmail.com>
2024-07-08 18:31:33 +0900

    Add : Base Class 추가

---
+++ SeeTheGood/SeeTheGood/Resource/Base/BaseTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  BaseTableViewCell.swift
+//  SeeTheGood
+//
+//  Created by 황민채 on 7/8/24.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .white
+        
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    func configureHierarchy() { }
+    func configureLayout() { }
+    func configureView() { }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ SeeTheGood/SeeTheGood/Resource/Base/BaseViewController.swift
@@ -0,0 +1,32 @@
+//
+//  BaseViewController.swift
+//  SeeTheGood
+//
+//  Created by 황민채 on 7/8/24.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    func configureHierarchy() {
+        
+    }
+    
+    func configureLayout() {
+        
+    }
+    
+    func configureView() {
+        
+    }
+}
