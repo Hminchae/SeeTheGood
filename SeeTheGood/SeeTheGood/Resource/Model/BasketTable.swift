051b79c02594bf29155afe5786a290fc0b27031d
Hminchae <hminchae@gmail.com>
2024-07-08 15:05:55 +0900

    Add : 목록 안에 basketTable 리스트를 저장하도록 테이블 추가, 변경

--- SeeTheGood/SeeTheGood/Resource/Model/BasketTable.swift
+++ SeeTheGood/SeeTheGood/Resource/Model/BasketTable.swift
@@ -9,28 +9,36 @@ import Foundation
 
 import RealmSwift
 
+class BasketCategory: Object {
+    @Persisted var categoryTitle: String
+    @Persisted var productList: List<BasketTable>
+}
+
+
 class BasketTable: Object {
     @Persisted(primaryKey: true) var id: ObjectId
-    @Persisted(indexed: true) var productId: Int
-    @Persisted var title: String
+    @Persisted(indexed: true) var title: String
+    @Persisted var productNum: Int
     @Persisted var link: String
-    @Persisted var image: String
+    @Persisted var imageUrl: String
     @Persisted var mallName: String
     @Persisted var price: String
     
-    convenience init(productId: Int,
-                     title: String,
+    convenience init(title: String,
+                     productNum: Int,
                      link: String,
-                     image: String,
+                     imageUrl: String,
                      mallName: String,
                      price: String
     ) {
         self.init()
-        self.productId = productId
         self.title = title
+        self.productNum = productNum
         self.link = link
-        self.image = image
+        self.imageUrl = imageUrl
         self.mallName = mallName
         self.price = price
     }
 }
+
+
